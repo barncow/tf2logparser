9eae36c38be9e6f36102a2cba43f005eda6b39d6
Brian Barnekow <brian.barnekow@gmail.com>
2011-06-10 14:47:59 -0500

    small change to logparser test - each test should create its own parser.

--- test/tf2logparser.test.js
+++ test/tf2logparser.test.js
@@ -1,8 +1,9 @@
 var should = require('should'),
-  parser = require('../lib/tf2logparser').create();
+  LogParser = require('../lib/tf2logparser');
   
 module.exports = {
   'can get log': function() {
+    var parser = LogParser.create();
     var log = parser.getLog();
     log.should.eql({});
   }
