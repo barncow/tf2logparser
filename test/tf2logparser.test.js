1dc50a20f1930063a788de8d0698610172c55157
Brian Barnekow <brian.barnekow@gmail.com>
2011-06-10 16:50:06 -0500

    Log parser can now read a file line by line (not loading the whole thing into memory)

--- test/tf2logparser.test.js
+++ test/tf2logparser.test.js
@@ -1,10 +1,30 @@
 var should = require('should'),
-  LogParser = require('../lib/tf2logparser');
+  LogParser = require('../lib/tf2logparser'),
+  FIXTURE_PATH = FP = './test/fixtures';
   
 module.exports = {
   'can get log': function() {
-    var parser = LogParser.create();
-    var log = parser.getLog();
+    var parser = LogParser.create(),
+      log = parser.getLog();
     log.should.eql({});
+  },
+  
+  'can get file from disk': function() {
+    var parser = LogParser.create();
+    parser.readFile(FP+'/blah.log', function(line) {
+      line.should.eql('blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah');
+    });
+  },
+  
+  'is only getting one line from file': function() {
+    var parser = LogParser.create(),
+      i = 0;
+    parser.readFile(FP+'/mini.log', function(line) {
+      //if the first two lines are OK, we will assume this is working as intended.
+      if(i == 0) line.should.eql('L 09/29/2010 - 19:05:47: Log file started (file "logs/L0929002.log") (game "/home/barncow/74.122.197.144-27015/srcds_l/orangebox/tf") (version "4317")');
+      else if(i == 1) line.should.eql('L 09/29/2010 - 19:05:47: server_cvar: "mp_falldamage" "0"');
+      
+      ++i;
+    });
   }
 }
