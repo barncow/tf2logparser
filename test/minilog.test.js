e926b6aab2b95d2ff21554e54fd2d06beda60084
Brian Barnekow <brian.barnekow@gmail.com>
2011-06-14 20:11:50 -0500

    Added support for team_say. Changed addupdateplayer to create properties on a cloned object, instead of mangling the original object (which should only have the info from the log itself. Broke minilog test into multiple assertions for somewhat easier issue resolving.

--- test/minilog.test.js
+++ test/minilog.test.js
@@ -16,16 +16,17 @@ module.exports = {
       
       log.should.be.ok;
       
-      log.should.eql({
-        blueScore: 0,
-        redScore: 0,
-        elapsedTime: 0,
-        gameSeconds: 0,
-        mapName: 'ctf_2fort', 
-        events: [
-          {type: 'kill',player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 53, steamid: 'STEAM_0:1:16481274', team: 'Blue', 'roles': []}, weapon: 'scattergun', customkill: false},
+      //broke out these assertions to help narrow down any potential problems.
+      
+      log.blueScore.should.eql(0);
+      log.redScore.should.eql(0);
+      log.elapsedTime.should.eql(0);
+      log.gameSeconds.should.eql(0);
+      log.mapName.should.eql('ctf_2fort');
+      log.events.should.eql([
+          {type: 'kill',player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 48, steamid: 'STEAM_0:1:16481274', team: 'Red'}, weapon: 'scattergun', customkill: false},
           {type: 'kill',player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 48, steamid: 'STEAM_0:1:16481274', team: 'Red'}, weapon: 'pistol_scout', customkill: false},
-          {type: 'kill',player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red', 'roles': []}, victim: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red', 'roles': []}, weapon: 'world', customkill: 'suicide'},
+          {type: 'kill',player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, victim: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, weapon: 'world', customkill: 'suicide'},
           {type: 'kill',player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 48, steamid: 'STEAM_0:1:16481274', team: 'Red'}, weapon: 'scattergun', customkill: false},
           {type: 'assist', player: {name: 'Cres', userid: 49, steamid: 'STEAM_0:0:8581157', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 48, steamid: 'STEAM_0:1:16481274', team: 'Red'}},
           {type: 'kill',player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'FSTNG! Barncow', userid: 48, steamid: 'STEAM_0:1:16481274', team: 'Red'}, weapon: 'scattergun', customkill: false},
@@ -35,10 +36,11 @@ module.exports = {
           {type: 'domination', player: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, victim: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}},
           {type: 'builtobject', player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, object: 'OBJ_SENTRYGUN'},
           {type: 'kill',player: {name: 'FSTNG! Barncow', userid: 53, steamid: 'STEAM_0:1:16481274', team: 'Blue'}, victim: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, weapon: 'minigun', customkill: 'OBJ_SENTRYGUN'},
-          {type: 'kill',player: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red', 'roles': []}, victim: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, weapon: 'pda_engineer', customkill: 'OBJ_TELEPORTER'},
+          {type: 'kill',player: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, victim: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, weapon: 'pda_engineer', customkill: 'OBJ_TELEPORTER'},
           {type: 'kill',player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, victim: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}, weapon: 'sniperrifle', customkill: 'headshot'},
           {type: 'revenge', player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, victim: {name: 'Target', userid: 46, steamid: 'STEAM_0:0:6845279', team: 'Blue'}},
-          {type: 'exinguished', player: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, victim: {name: '`yay!', userid: 52, steamid: 'STEAM_0:0:973270', team: 'Blue', 'roles': []}, weapon: 'tf_weapon_medigun'},
+          {type: 'say_team', player: {name: '[!?] cheap', userid: 56, steamid: 'STEAM_0:0:12272740', team: 'Blue'}, text: 'I can also play pyro. I have been doing that a lot on 2fort and doublecross.'},
+          {type: 'exinguished', player: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, victim: {name: '`yay!', userid: 52, steamid: 'STEAM_0:0:973270', team: 'Red'}, weapon: 'tf_weapon_medigun'},
           {type: 'chargedeployed', player: {name: 'FSTNG! Barncow', userid: 53, steamid: 'STEAM_0:1:16481274', team: 'Blue'}},
           {type: 'kill',player: {name: '[H2K]BubbleAlan ʚϊɞ', userid: 55, steamid: 'STEAM_0:0:556497', team: 'Red'}, victim: {name: '[H2K]BubbleAlan ʚϊɞ', userid: 55, steamid: 'STEAM_0:0:556497', team: 'Red'}, weapon: 'world', customkill: 'suicide', 'healing': 160, ubercharge: false},
           {type: 'kill',player: {name: 'Ctrl+f Muffin!', userid: 50, steamid: 'STEAM_0:1:9852193', team: 'Red'}, victim: {name: 'FSTNG! Barncow', userid: 53, steamid: 'STEAM_0:1:16481274', team: 'Blue'}, weapon: 'sniperrifle', customkill: false, healing: 1800, ubercharge: true},
@@ -48,59 +50,59 @@ module.exports = {
           {type: 'assist', player: {name: 'Cres', userid: 49, steamid: 'STEAM_0:0:8581157', team: 'Blue'}, victim: {name: '`yay!', userid: 52, steamid: 'STEAM_0:0:973270', team: 'Red'}},
           {type: 'kill',player: {name: '`yay!', userid: 52, steamid: 'STEAM_0:0:973270', team: 'Blue'}, victim: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, weapon: 'sadfsgtghgher', customkill: false},
           {type: 'kill',player: {name: '`yay!', userid: 52, steamid: 'STEAM_0:0:973270', team: 'Blue'}, victim: {name: 'perl', userid: 57, steamid: 'STEAM_0:0:11710749', team: 'Red'}, weapon: 'sadfsgtghgher', customkill: false}
-        ],
-        players: [
-          { 
-            name: 'Target',
-            userid: 46,
-            steamid: 'STEAM_0:0:6845279',
-            team: 'Red',
-            roles: [{role: 'scout'}]
-          },
-          {
-            name: 'FSTNG! Barncow',
-            userid: 53,
-            steamid: 'STEAM_0:1:16481274',
-            team: 'Blue',
-            roles: []
-          },
-          { 
-            name: 'perl',
-            userid: 57,
-            steamid: 'STEAM_0:0:11710749',
-            team: 'Red',
-            roles: []
-          },
-          { 
-            name: '[!?] cheap',
-            userid: 56,
-            steamid: 'STEAM_0:0:12272740',
-            team: 'Blue',
-            roles: []
-          },
-          { 
-            name: '`yay!',
-            userid: 52,
-            steamid: 'STEAM_0:0:973270',
-            team: 'Blue',
-            roles: []
-          },
-          { 
-            name: 'ǤooB',
-            userid: 54,
-            steamid: 'STEAM_0:1:23384772',
-            team: 'Spectator',
-            roles: []
-          },
-          { 
-            name: '[H2K]BubbleAlan ʚϊɞ',
-            userid: 55,
-            steamid: 'STEAM_0:0:556497',
-            team: 'Spectator',
-            roles: []
-          }
-        ]
-      });
-    });
+        ]);
+        
+      log.players.should.eql([
+        { 
+          name: 'Target',
+          userid: 46,
+          steamid: 'STEAM_0:0:6845279',
+          team: 'Red',
+          roles: [{role: 'scout'}]
+        },
+        {
+          name: 'FSTNG! Barncow',
+          userid: 53,
+          steamid: 'STEAM_0:1:16481274',
+          team: 'Blue',
+          roles: []
+        },
+        { 
+          name: 'perl',
+          userid: 57,
+          steamid: 'STEAM_0:0:11710749',
+          team: 'Red',
+          roles: []
+        },
+        { 
+          name: '[!?] cheap',
+          userid: 56,
+          steamid: 'STEAM_0:0:12272740',
+          team: 'Blue',
+          roles: []
+        },
+        { 
+          name: '`yay!',
+          userid: 52,
+          steamid: 'STEAM_0:0:973270',
+          team: 'Blue',
+          roles: []
+        },
+        { 
+          name: 'ǤooB',
+          userid: 54,
+          steamid: 'STEAM_0:1:23384772',
+          team: 'Spectator',
+          roles: []
+        },
+        { 
+          name: '[H2K]BubbleAlan ʚϊɞ',
+          userid: 55,
+          steamid: 'STEAM_0:0:556497',
+          team: 'Spectator',
+          roles: []
+        }
+      ]);      
+    }); //end parseFile callback
   }
 }
