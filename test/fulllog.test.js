4918a71a081cd569bcf70f3df250151dd4129536
Brian Barnekow <brian.barnekow@gmail.com>
2011-07-06 20:51:09 -0500

    got plupward test working, along with team switch detection.

--- test/fulllog.test.js
+++ test/fulllog.test.js
@@ -4,6 +4,7 @@ var should = require('should'),
   
 module.exports = { 
  '1123dwidgranary': function() {
+    //note - this log seems to start in the middle of a round. the first capture by red currently does not count until the "round_start"
     var parser = LogParser.create();
     parser.config.ignoreUnrecognizedLines = false;
     parser.parseLogFile(FP+'/full_1123dwidgranary.log', function(err, log) {
@@ -23,6 +24,17 @@ module.exports = {
       log.should.be.ok;
       log.blueScore.should.eql(7);
       log.redScore.should.eql(2);
+    });
+  },
+  'plupward': function() {
+    var parser = LogParser.create();
+    parser.config.ignoreUnrecognizedLines = false;
+    parser.parseLogFile(FP+'/full_plupward.log', function(err, log) {
+      if(err) console.log(err.stack);
+      should.not.exist(err);   
+      log.should.be.ok;
+      log.blueScore.should.eql(4);
+      log.redScore.should.eql(0);
     });
   }
 }
